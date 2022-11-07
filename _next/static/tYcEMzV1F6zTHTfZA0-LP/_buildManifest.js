b06b77892fdf0a131d1ec43c6f61adba8c307bc3
kbaranova <82108124+kbaranova@users.noreply.github.com>
2022-11-07 15:04:53 +0000

    Deploying to gh-pages from @ kbaranova/portfolio@7adb70e8fdf1efce67529c3c9de44e80f6149f99 🚀

---
+++ _next/static/tYcEMzV1F6zTHTfZA0-LP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/112-1e1b9233c2671fe5.js","static/chunks/pages/index-55857989e991576f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
